cbafd257d3ca492ac4fd00e9972b4b5655b39e8e
moon-raon <ma20arif@gmail.com>
2025-12-07 12:04:41 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition of two numbers is ",c)
+print("subtraction :",d)
+print("multiplication :",e)
+print("division :",f)
+
---
+++ task2.py
@@ -0,0 +1,4 @@
+s=input("enter first name ")
+j=input("enter last name ")
+a=s + j
+print("Hello! ",a,"welcome to python world")
